4f01aef1a17735c4dbebf8519c848ff85e05650d
LongStarEVXK <hzheng15@ualberta.ca>
2023-10-06 15:23:37 -0600

    Revert "Merge branch 'main' into Haoyu_Zheng"
    This reverts commit b1de62cab95fe7057cd570465648b7d6e74d1397, reversing
    changes made to 525ca8ed41ab3edaa24a8c14e795dd70fb733e55.

--- app/src/main/java/com/example/myapplication/Circle.java
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication;
-
-public class Circle extends Shape {
-    int radius;
-}
--- app/src/main/java/com/example/myapplication/Square.java
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication;
-
-public class Square extends Shape{
-
-}
